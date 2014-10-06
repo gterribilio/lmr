e57dca0a2341d6ba814f6bdd3b5ab7741f024972
unknown <administrator@SRVVIRTUAL16.izsto.it>
2014-10-06 10:00:54 +0200

    ''

--- web.js
+++ web.js
@@ -1,18 +1,8 @@
-var express = require("express");
-var logfmt = require("logfmt");
+var gzippo = require('gzippo');
+var express = require('express');
+var morgan = require('morgan');
 var app = express();
 
-app.use(logfmt.requestLogger());
-
-app.use(express.static(__dirname + '/app'));
-
-
-app.get('/', function(req, res) {
-  //res.send('Hello World!');
-  res.sendfile('./app/index.html');
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
