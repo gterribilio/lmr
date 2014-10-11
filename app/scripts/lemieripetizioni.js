debed01c4100127dde95010b59785347ea0c8560
gterribilio <gterribilio@libero.it>
2014-10-11 17:06:55 +0200

    'o'

--- app/scripts/lemieripetizioni.js
+++ app/scripts/lemieripetizioni.js
@@ -165,18 +165,6 @@ var marker = new google.maps.Marker({
     animation: google.maps.Animation.DROP
 });
 
-
-function toggleBounce() {
-
-  if (marker.getAnimation() != null) {
-    marker.setAnimation(null);
-} else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-}
-}
-
-google.maps.event.addListener(marker, 'click', toggleBounce);
-
 }
 
 function loadScript() {
@@ -192,10 +180,54 @@ window.onload = loadScript;
 /*ANIMATION DEL FOOTER PER FADE IN - FADE - OUT*/
 $(window).scroll(function () {
     var y = $(window).scrollTop(),
-        x = $('.animated').offset().top - 150;
+        x = $('.animated').offset().top - 200;
     if (y > x) {
         $('.animated').addClass('fadeInUp').removeClass('fadeOutDown');
     } else {
         $('.animated').removeClass('fadeInUp').addClass('fadeOutDown');
     }
+});
+
+/*REGISTRATION*/
+
+$(function() {   
+    $('input .input-group input[required], .input-group textarea[required], .input-group select[required]').on('keyup change', function() {
+        var $form = $(this).closest('form'),
+            $group = $(this).closest('.input-group'),
+            $addon = $group.find('.input-group-addon'),
+            $icon = $addon.find('span'),
+            state = false;
+            
+        if (!$group.data('validate')) {
+            state = $(this).val() ? true : false;
+        }else if ($group.data('validate') == "email") {
+            state = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test($(this).val())
+        }else if($group.data('validate') == 'phone') {
+            state = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/.test($(this).val())
+        }else if ($group.data('validate') == "length") {
+            state = $(this).val().length >= $group.data('length') ? true : false;
+        }else if ($group.data('validate') == "number") {
+            state = !isNaN(parseFloat($(this).val())) && isFinite($(this).val());
+        }
+
+        if (state) {
+                $addon.removeClass('danger');
+                $addon.addClass('success');
+                $icon.attr('class', 'glyphicon glyphicon-ok');
+        }else{
+                $addon.removeClass('success');
+                $addon.addClass('danger');
+                $icon.attr('class', 'glyphicon glyphicon-remove');
+        }
+        
+        if ($form.find('.input-group-addon.danger').length == 0) {
+            $form.find('[type="submit"]').prop('disabled', false);
+        }else{
+            $form.find('[type="submit"]').prop('disabled', true);
+        }
+    });
+    
+    $('.input-group input[required], .input-group textarea[required], .input-group select[required]').trigger('change');
+    
+    
 });
